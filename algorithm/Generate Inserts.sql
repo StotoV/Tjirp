b871b20e751d79fc228f62b1fac8be9be2a41fc6
Bart Kessels <bartkessels@bk-mail.com>
2018-01-15 10:02:03 +0100

    Add insert script for foreign keys

--- algorithm/Generate Inserts.sql
+++ algorithm/Generate Inserts.sql
@@ -48,16 +48,17 @@ ORDER BY
 -- Foreign keys
 --
 SELECT
-    'INSERT INTO ',
-    OBJECT_NAME (fk.object_id) as constraintName,
-    OBJECT_NAME (fk.parent_object_id) as tableName,
-    COL_NAME (fk_columns.parent_object_id, fk_columns.parent_column_id) as columnName,
-    OBJECT_NAME (fk_columns.referenced_object_id) as referencedTableName,
-    COL_NAME (fk_columns.referenced_object_id, fk_columns.referenced_column_id) as referencedColumnName
+    'INSERT INTO DeclarativeConstraint (tableName, columnName, constraintName, constraintType, constraintLogic) VALUES (' +
+      '''' + OBJECT_NAME (fk.parent_object_id) + ''',' +                                                                -- Table name
+      '''' + COL_NAME (fk_columns.parent_object_id, fk_columns.parent_column_id) + ''',' +                              -- Column name
+      '''' + OBJECT_NAME (fk.object_id) + ''',' +                                                                       -- Constraint name
+      '''foreignKey'', ' +
+      '''ALTER TABLE ' + OBJECT_NAME (fk.parent_object_id) + ' ADD CONSTRAINT ' + OBJECT_NAME (fk.object_id) + ' ' +    -- Table name + constraint name
+      'FOREIGN KEY (' + COL_NAME (fk_columns.parent_object_id, fk_columns.parent_column_id) + ') ' +                    -- Column name on which the foreign key is placed
+      'REFERENCES ' + OBJECT_NAME (fk_columns.referenced_object_id) +                                                   -- Referenced table name
+      ' (' + COL_NAME (fk_columns.referenced_object_id, fk_columns.referenced_column_id) + ')'');'                      -- column name in referenced table
 FROM
     sys.foreign_keys fk INNER JOIN sys.foreign_key_columns fk_columns
     ON fk.OBJECT_ID = fk_columns.constraint_object_id
     INNER JOIN sys.tables tables
     ON fk_columns.referenced_object_id = tables.object_id
-ORDER BY
-    tableName
